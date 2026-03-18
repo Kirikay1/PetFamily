f95b563dbcc5fd7de57294fd46854269179c69ef
Maxim <maksim.timoshin.045@gmail.com>
2026-03-19 02:11:53 +0300

    feat: create model pet

---
+++ PetFamily.Backend/src/PetFamily.Domain/Pet/Requisites.cs
@@ -0,0 +1,11 @@
+﻿namespace PetFamily.Domain.Pet
+{
+    public class Requisites
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = default!;
+
+        public string Description { get; set; } = default!;
+    }
+}
---
+++ PetFamily.Backend/src/PetFamily.Domain/Pet/Species/Species.cs
@@ -0,0 +1,11 @@
+﻿namespace PetFamily.Domain.Pet.Species
+{
+    public class Species
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = default!;
+
+        public List<Breed> Breeds { get; set; } = [];
+    }
+}
