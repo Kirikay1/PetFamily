6a1db36856dfe50c154bd3390a0007552363210f
Maksim <maksim.timoshin.045@gmail.com>
2026-06-05 19:45:06 +0300

    start B-4.1 add dbcontext and postgres setup

---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PetFamily.Domain.Pets;
+using PetFamily.Domain.Pets.Species;
+using PetFamily.Domain.Volunteers;
+using static CSharpFunctionalExtensions.Result;
+
+namespace PetFamily.Infrastructure
+{
+    public class ApplicationDbContext(IConfiguration configuration) : DbContext
+    {
+        private const string DATABASE = "DataBase";
+
+        public DbSet<Pet> Pets => Set<Pet>();
+        public DbSet<Volunteer> Volunteers => Set<Volunteer>();
+        public DbSet<Breed> Breeds => Set<Breed>();
+        public DbSet<Species> Species => Set<Species>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString(DATABASE));
+            optionsBuilder.UseSnakeCaseNamingConvention();
+        }
+
+    }
+}
